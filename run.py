ca41af0b7f654c116e4efc04995e67aa1b927a72
rebahama <wiri@hotmail.se>
2022-04-05 04:14:52 +0000

    try inserting data to spreadsheet

--- run.py
+++ run.py
@@ -15,28 +15,54 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('employee-add')
 # acess spreadsheet data
-employee=SHEET.worksheet('employee')
+employee = SHEET.worksheet('employee')
 
-data_employee=employee.get_all_values()
+
+data_employee = employee.get_all_values()
+print(data_employee)
 
 
 class Employee:
  def __init__(self, id_nr, name, age, country):
-        self.id_nr=id_nr
-        self.name=name
-        self.age=age
-        self.country=country
+        self.id_nr = id_nr
+        self.name = name
+        self.age = age
+        self.country = country
         
  def add_input(self):
    
-    self.id_nr=int(input('write employ nr please'))
-    self.name=(input('write name please'))
-    self.age=int(input('write age please'))
-    self.country=(input('write country please'))
+    self.id_nr = int(input('write employ nr please'))
+    self.name = (input('write name please'))
+    self.age = int(input('write age please'))
+    self.country = (input('write country please'))
 
  def data_output(self):
-    return f"You have added --- Employe-id: {self.id_nr} ---- Name: {self.name} --- Age: {self.age} --- Country: {self.country}"
+    return f"You have added --- Employe-id: {self.id_nr} ---- Name: {self.name} --- Age: {self.age} --- Country: {self.country} to the employee database"
 
-e1=Employee(3, "swen", 3, "swden")
-e1.add_input()
-print(e1.data_output())
+e1=Employee(0, "", 0, "")
+
+def update(data):
+ 
+ add_employee=SHEET.worksheet("employee")
+ add_employee.append_row(data)
+
+
+
+#def add_numbers():
+#num1=input('add a word')
+
+
+num1=input('add a word')
+sales_data=[int(num) for num in num1]
+update(sales_data)
+print(num1)
+
+
+
+
+#e1.add_input()
+#print(e1.data_output())
+
+
+
+
