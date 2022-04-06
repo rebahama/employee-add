9a7b10ee574ffcb4b484d6d4f9f93df94ec4cd82
rebahama <wiri@hotmail.se>
2022-04-06 01:30:33 +0000

    complete the add_data function

--- run.py
+++ run.py
@@ -23,22 +23,23 @@ print(data_employee)
 
 
 class Employee:
- def __init__(self, id_nr, name, age, country):
+    def __init__(self, id_nr, name, age, country):
         self.id_nr = id_nr
         self.name = name
         self.age = age
         self.country = country
         
- def add_input(self):
+    def add_input(self):
    
-    self.id_nr = int(input('write employ nr please'))
-    self.name = (input('write name please'))
-    self.age = int(input('write age please'))
-    self.country = (input('write country please'))
+        self.id_nr = int(input('write employ nr please'))
+        self.name = (input('write name please'))
+        self.age = int(input('write age please'))
+        self.country = (input('write country please'))
 
- def data_output(self):
-    return f"You have added --- Employe-id: {self.id_nr} ---- Name: {self.name} --- Age: {self.age} --- Country: {self.country} to the employee database"
+    def data_output(self):
+        return f"You have added --- Employe-id: {self.id_nr} ---- Name: {self.name} --- Age: {self.age} --- Country: {self.country} to the employee database"
 
+    
 e1=Employee(0, "", 0, "")
 
 def update(data):
@@ -54,21 +55,26 @@ def update(data):
 empty=[]
 
 
-
-n1=1
-for i in range(0, n1):
+def add_data_spreadsheet():
+ n1=1
+ for i in range(0, n1):
+    e1.id_nr=input('add an Employee ID')
     e1.name=input('add a name')
-    e1.age=input(('add age'))
+    e1.age=input('add a age')
+    e1.country=input('add a country')
+    
    
+    empty.append(e1.id_nr)
     empty.append(e1.name)
     empty.append(e1.age)
+    empty.append(e1.country)
     
-
+add_data_spreadsheet()
 
 sales_data=[str(num) for num in empty]
 
 update(sales_data)
-
+print(e1.data_output())
 print(empty)
 
 
