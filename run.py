d034471a3dc3d371782597d4ca4bb8dac03b9f09
rebahama <wiri@hotmail.se>
2022-04-06 04:31:27 +0000

    Refactor: remove docstring text

--- run.py
+++ run.py
@@ -17,9 +17,9 @@ SHEET = GSPREAD_CLIENT.open('employee-add')
 # acess spreadsheet data
 employee = SHEET.worksheet('employee')
 
+
+
 class Employee:
-    """ Create a class for putting an object inside with attribute variables
-    """
     def __init__(self, id_nr, name, age, country):
         self.id_nr = id_nr
         self.name = name
@@ -27,51 +27,41 @@ class Employee:
         self.country = country
         
     def add_input(self):
-        """ input for storing data inside the attributes from the init function
-        """
+   
         self.id_nr = int(input('write employ nr please'))
         self.name = input('write name please')
         self.age = int(input('write age please'))
         self.country = input('write country please')
 
     def data_output(self):
-        """ function for returning the input data that was stored
-        """
         return f"You have added --- Employe-id: {self.id_nr} ---- Name: {self.name} --- Age: {self.age} --- Country: {self.country} to the employee database"
 
     
-#create an object of the class 
 e1=Employee(0, "", 0, "")
 
 def update(data):
-    """ append data inside the worksheet named "employee"
-    """
     add_employee=SHEET.worksheet("employee")
     add_employee.append_row(data)
 
-# list for storing data from the spreadsheet
+
 empty=[]
 
 
 def add_data_spreadsheet():
-    """ adding the data requested from the user inside the variables in class Employee
-    """
-n1=1
-for i in range(0, n1):
+ n1=1
+ for i in range(0, n1):
     e1.id_nr=input ('add an Employee ID:')
     e1.name=input ('add a Name:')
     e1.age=input ('add an Age:')
     e1.country=input ('add a Country:')
     
-   #put the variables inside the empty list
+   
     empty.append(e1.id_nr)
     empty.append(e1.name)
     empty.append(e1.age)
     empty.append(e1.country)
     
 def request_data():
-    """ show all the data that have been saved in the spreadsheet from the user
-    """
     values=SHEET.worksheet('employee').get_all_values()
     value=SHEET.worksheet('employee').get_all_values()[0]
     
@@ -87,10 +77,10 @@ def request_data():
    
 
 
-add_data_spreadsheet()
-sales_data = [str(num) for num in empty]
+#add_data_spreadsheet()
+#sales_data = [str(num) for num in empty]
 
-update(sales_data)
-print(e1.data_output())
+#update(sales_data)
+#print(e1.data_output())
 
 request_data()
